431954c4d606b0215668ff38f7da7fc2767dcdce
madziaz <madziaz.mz@gmail.com>
2021-10-21 16:39:09 +0200

    Lab03

--- app/src/main/java/com/example/lab2_magdalenazdun/MainActivity.java
+++ app/src/main/java/com/example/lab2_magdalenazdun/MainActivity.java
@@ -1,34 +1,40 @@
 package com.example.lab2_magdalenazdun;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+public class MainActivity extends FragmentActivity{
 
-public class MainActivity extends AppCompatActivity {
-
-    private FragmentManager fragmentManager;
-    private Fragment fragment1, fragment2, fragment3, fragment4;
+    private int[] frames;
+    private boolean hiden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
-
-        fragmentManager = getSupportFragmentManager();
-
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frame1, fragment1);
-        transaction.add(R.id.frame2, fragment2);
-        transaction.add(R.id.frame3, fragment3);
-        transaction.add(R.id.frame4, fragment4);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        if (savedInstanceState == null) {
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hiden = false;
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            for (int i = 0; i < 4; i++) {
+                transaction.add(frames[i], fragments[i]);
+            }
+            transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hiden = savedInstanceState.getBoolean("HIDEN");
+        }
     }
-}
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDEN", hiden);
+    }
